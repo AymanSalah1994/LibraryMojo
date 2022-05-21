f0257d3dd4b003d56f1348aa7ddf4b646b3bee24
Ayman Salah <ayman.1551.salah@gmail.com>
2022-05-21 08:57:15 +0200

    Basic Authentication Finished

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render 
-from django.urls import reverse_lazy
+from django.shortcuts import render  ,redirect
+from django.urls import reverse_lazy 
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 from django.views.generic import CreateView
@@ -7,3 +7,6 @@ from django.views.generic import CreateView
 class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = 'signup.html'
+    # success_url = redirect('login')
+    def get_success_url(self) -> str:
+        return reverse_lazy('login')
