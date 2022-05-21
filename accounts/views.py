e45406a795285e0ce19249edf26402a7297e9a95
Ayman Salah <ayman.1551.salah@gmail.com>
2022-05-21 06:54:33 +0200

    Sign up View

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+from django.views.generic import CreateView
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'signup.html'
